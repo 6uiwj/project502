890316df3f07e75ad4099bf42e09f4acd3f930af
6uiwj <oterges00@gmail.com>
2023-12-29 15:22:23 +0900

    231228

--- src/main/java/org/choongang/commons/Utils.java
+++ src/main/java/org/choongang/commons/Utils.java
@@ -1,23 +1,37 @@
 package org.choongang.commons;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 @RequiredArgsConstructor
 public class Utils {
 
     private final HttpServletRequest request;
+    private final HttpSession session;
 
     public boolean isMobile() {
+        //모바일 수동전환 모드 체크
+        String device = (String)session.getAttribute("device");
+        if(StringUtils.hasText(device))  {
+            return device.equals("MOBILE");
+        }
+
         //요청 헤더: User-Agent 정보를 가지고 모바일인지 아닌지 체크할 것임
         String ua = request.getHeader("User-Agent");
         String pattern = ".*(iPhone|iPod|iPad|BlackBerry|Android|Windows CE|LG|MOT|SAMSUNG|SonyEricsson).*";
 
-        boolean isMobile = ua.matches(pattern);
+        return ua.matches(pattern);
 
-        return isMobile;
+    }
+
+    public String tpl(String path) {
+        String prefix = isMobile() ? "mobile/" : "front/";
+
+        return prefix + path;
     }
 
 }
