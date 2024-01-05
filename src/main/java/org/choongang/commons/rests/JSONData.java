10459c144f46d118f274a7cb71fbf0526968a318
6uiwj <oterges00@gmail.com>
2024-01-05 17:50:59 +0900

    240105 망한파일입니다.

--- src/main/java/org/choongang/commons/rests/JSONData.java
+++ src/main/java/org/choongang/commons/rests/JSONData.java
@@ -1,17 +1,20 @@
 package org.choongang.commons.rests;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 //많이 쓰는 부분은 값을 고정
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class JSONData<T> {
     private HttpStatus status = HttpStatus.OK;
     private boolean success = true; //성공여부
+
+    @NonNull //데이터는 상수가 아니라 바꿀 수 있기 때문에 nonnull을ㄴ ㅓㅎ는다구...?
     private T data; //성공시데이터
     private String message;
 }
--- src/main/java/org/choongang/member/MemberUtil.java
+++ src/main/java/org/choongang/member/MemberUtil.java
@@ -2,6 +2,7 @@ package org.choongang.member;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.choongang.member.entities.Authorities;
 import org.choongang.member.entities.Member;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,16 @@ import org.springframework.stereotype.Component;
 public class MemberUtil {
 
     private final HttpSession session;
+
+    public boolean isAdmin() {
+        if(isLogin()) {
+            return getMember().getAuthorities()
+                    .stream().map(Authorities::getAuthority)
+                    .anyMatch(a -> a == Authority.ADMIN || a == Authority.MANAGER);
+        }
+        return false;
+    }
+
     //세션데이터가 있으면 로그인한 것 / 세션데이터가 없으면 로그인 X
     public boolean isLogin() {
         return getMember() != null;
