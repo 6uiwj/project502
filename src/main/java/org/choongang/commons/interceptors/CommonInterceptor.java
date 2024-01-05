c022e15fec654f00821b18d624dc7fb6ce5f0e17
6uiwj <oterges00@gmail.com>
2024-01-05 09:25:54 +0900

    240105 css경로수정

--- src/main/java/org/choongang/commons/interceptors/CommonInterceptor.java
+++ src/main/java/org/choongang/commons/interceptors/CommonInterceptor.java
@@ -3,19 +3,27 @@ package org.choongang.commons.interceptors;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.choongang.admin.controllers.BasicConfig;
+import org.choongang.admin.sersvice.ConfigInfoService;
 import org.choongang.member.MemberUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.Arrays;
+
 //공통 인터셉터
 @Component
+@RequiredArgsConstructor
 public class CommonInterceptor implements HandlerInterceptor {
+    private final ConfigInfoService infoService;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         checkDevice(request);
         clearLoginData(request);
+        loadSiteConfig(request);
         return true;
     }
 
@@ -48,5 +56,21 @@ public class CommonInterceptor implements HandlerInterceptor {
             HttpSession session = request.getSession();
             MemberUtil.clearLoginData(session);
         }
+    }
+
+    private void loadSiteConfig(HttpServletRequest request) {
+        String[] excludes = {".js", ".css", ".png", ".jpg", ".jpeg", "gif", ".pdf", ".xls",
+        ".xlxs", ".ppt"}; //URL에 이런 파일이 추가되었다면 이 파일들은 설정을 공유할 필요가 없다?(로드배제)
+
+        String URL = request.getRequestURI().toLowerCase();
+        boolean isIncluded = Arrays.stream(excludes).anyMatch(s -> URL.contains(s));
+        if (isIncluded) {
+            return;
+        }
+
+        BasicConfig config = infoService.get("basic", BasicConfig.class)
+            .orElseGet(BasicConfig::new);
+            request.setAttribute("siteConfig", config);
+
     }
 }
