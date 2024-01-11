54eb4aec27d2688ea99401b8a8f0e3a99ba0ede2
6uiwj <oterges00@gmail.com>
2024-01-12 01:37:45 +0900

    240112 헌혈의집 엔티티 & 예약 엔티티

--- src/main/java/org/choongang/reservation/constants/DonationType.java
+++ src/main/java/org/choongang/reservation/constants/DonationType.java
@@ -1,6 +1,6 @@
 package org.choongang.reservation.constants;
-
+//이넘클래스
 public enum DonationType {
     //전혈 = TYPE_A.. 나중에 명칭 바꾸기
-    TYPE_A, TYPE_B;
+    TYPE_A, TYPE_B, TYPE_C; //전혈, 혈소판, 혈장으로 바꾸기
 }
--- src/main/java/org/choongang/reservation/entities/Reservation.java
+++ src/main/java/org/choongang/reservation/entities/Reservation.java
@@ -11,24 +11,25 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
-//날짜와시간이 필요하므로 상속
+//날짜와시간이 필요하므로 Base 상속
 public class Reservation extends Base {
     @Id @GeneratedValue
     private Long bookCode; //예약 코드
 
-    //한명이 여러군데에 예약할 수있으니까...
+    //한명이 여러군데에 예약할 수 있으니까...
     @ManyToOne (fetch = FetchType.LAZY)
-    @JoinColumn(name="member_seq")
-    private Member member;
+    @JoinColumn(name="member_seq") //이거확인
+    private Member member; //회원은 회원쪽 매핑
 
     @Column(length=15, nullable = false)
     private String donnerTel; //헌혈자 전화번호 /로그인화면이면 완성시키고 ,바꿀 수있도록 설정
 
-    //헌혈타입은 상수로 빼준다.
+    //헌혈타입은 상수로 빼준다. (전혈, 혈장, 혈소판)
     @Enumerated(EnumType.STRING)
     @Column(length=25, nullable = false)
     private DonationType bookType;
 
+    //한 헌혈의집에 여러명의 예약 가능
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="cCode")
     private Center center;
